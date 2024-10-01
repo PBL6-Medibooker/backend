0553dc2585ef6109afb4bd58db07421a5c1f9aa2
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-10-01 07:34:23 +0700

    / redirect to home

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const speciality_Routes = require('./speciality_Routes')
 const region_Routes = require('./region_Routes')
 
 function routes(app){
-    app.use("/")
+    app.use("/", (res, req) =>{
+        res.redirect("/Home")
+    })
     app.use("/acc", account_Routes)
     app.use("/special", speciality_Routes)
     app.use("/region", region_Routes)
