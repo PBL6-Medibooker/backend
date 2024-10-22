66c5d041b941c8dce0586d22c265225c1565ce38
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-10-22 07:24:15 +0700

    add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 const account_Routes = require("./account_Routes")
 const speciality_Routes = require('./speciality_Routes')
 const region_Routes = require('./region_Routes')
+const client_Routes = require('./client_Routes')
+const appointment_Routes = require('./appointment_Routes')
 
 function routes(app){
     app.use("/acc", account_Routes)
     app.use("/special", speciality_Routes)
     app.use("/region", region_Routes)
+    app.use("/client", client_Routes)
+    app.use("/appointment", appointment_Routes)
 }
 
 module.exports = routes
