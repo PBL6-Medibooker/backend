b793439d221cdecc65be7e3b74022adf27d20c94
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-10-23 17:18:14 +0700

    login return id

--- src/app/controllers/account_Controller.js
+++ src/app/controllers/account_Controller.js
@@ -49,15 +49,15 @@ class user_Controller{
         try{
             let acc
             acc = await User.login(email, password)
-
+            const acc_id = acc._id
             const token = this.create_Token(acc._id)
             const role = acc.role
             
             if(acc instanceof Doctor){
                 const verified = acc.verified
-                res.status(200).json({email, token, role, verified})
+                res.status(200).json({acc_id, email, token, role, verified})
             }else{
-                res.status(200).json({email, token, role})
+                res.status(200).json({acc_id, email, token, role})
             }
         }catch(error){
             console.log(error.message)
