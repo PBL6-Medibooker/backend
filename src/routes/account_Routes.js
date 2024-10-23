7da14b41b9a7a1a2d496386bba9b1ab115ee3c81
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-10-23 19:08:03 +0700

    change so request from get to post

--- src/routes/account_Routes.js
+++ src/routes/account_Routes.js
@@ -6,8 +6,8 @@ const router = express.Router()
 
 router.post('/login', account_Controller.acc_Login)
 router.post('/signup', account_Controller.acc_Signup)
-router.get('/acc-list', account_Controller.get_Account_List)
-router.get('/get-acc', account_Controller.get_Account)
+router.post('/acc-list', account_Controller.get_Account_List)
+router.post('/get-acc', account_Controller.get_Account)
 router.post('/update-acc-info/:id', account_Controller.update_Acc_Info)
 router.post('/soft-delete-acc', account_Controller.soft_Delete_Account)
 router.post('/perma-delete-acc', account_Controller.perma_Delete_Account)
@@ -19,7 +19,7 @@ router.get('/active-hour-list/:id', account_Controller.get_Doctor_Active_Hour_Li
 router.post('/add-active-hour/:id', account_Controller.add_Doctor_Active_Hour)
 router.post('/update-active-hour/:id', account_Controller.update_Doctor_Active_Hour)
 router.post('/delete-active-hour/:id', account_Controller.delete_Doctor_Active_Hour)
-router.get('/filter-doctor-list', account_Controller.get_Filtered_Doctor_List)
+router.post('/filter-doctor-list', account_Controller.get_Filtered_Doctor_List)
 
 module.exports = router
 
