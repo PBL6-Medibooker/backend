1fececd8e8668cda7c60800128124f1b907d897a
NguyenHuyNhan2003 <124428721+NguyenHuyNhan2003@users.noreply.github.com>
2024-11-16 12:01:18 +0700

    Update appointment_Routes.js

--- src/routes/appointment_Routes.js
+++ src/routes/appointment_Routes.js
@@ -1,7 +1,7 @@
-const appointment_Controller = require("../app/controllers/appointment_Controller")
-const require_Auth = require("../middleware/require_Auth")
+const appointment_Controller = require('../app/controllers/appointment_Controller')
+const require_Auth = require('../middleware/require_Auth')
 
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
 
