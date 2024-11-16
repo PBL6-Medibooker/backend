77c59456ab926ea860d363897a56777f595abf0a
NguyenHuyNhan2003 <124428721+NguyenHuyNhan2003@users.noreply.github.com>
2024-11-16 12:09:34 +0700

    Update account_Controller.js

--- src/app/controllers/account_Controller.js
+++ src/app/controllers/account_Controller.js
@@ -48,6 +48,24 @@ class user_Controller{
         return jwt.sign({_id}, process.env.JWTSecret, {expiresIn})
     }
 
+    loginAdmin = async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            if (
+                email === process.env.ADMIN_EMAIL &&
+                password === process.env.ADMIN_PASSWORD
+            ) {
+                const token = jwt.sign(email + password, process.env.JWTSecret);
+                res.json({ success: true, token });
+            } else {
+                res.json({ success: false, message: "Invalid credentials" });
+            }
+        } catch (error) {
+          console.log(error);
+          res.json({ success: false, message: error.message });
+        }
+    };
+
     acc_Login = async(req, res) => {
         // get info from body
         const {email, password} = req.body
