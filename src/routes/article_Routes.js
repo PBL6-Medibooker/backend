2c630bd16b13b2de0a37996c9d1817c30f1bf408
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-11-17 13:53:17 +0700

    fix perma-del-article

--- src/routes/article_Routes.js
+++ src/routes/article_Routes.js
@@ -12,7 +12,7 @@ router.post('/get-all-article-by-speciality', article_Controller.get_all_Article
 router.post('/update-article/:id', article_Controller.update_Article)
 router.post('/soft-del-article', article_Controller.soft_Delete_Article)
 router.post('/restore-article', article_Controller.restore_Article)
-router.post('/perma-del-article', article_Controller.soft_Delete_Article)
+router.post('/perma-del-article', article_Controller.perma_Delete_Article)
 router.post('/search-article', article_Controller.search_Article_By_Title_and_Content)
 
 module.exports = router
