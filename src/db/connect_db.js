c3c9cfe4044588812659424241f578d4e712a582
NguyenHuyNhan2003 <nguyenhuynhan.dn@gmail.com>
2024-12-22 15:30:34 +0700

    change email html formats

--- src/db/connect_db.js
+++ src/db/connect_db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 async function connect() {
     try{
-        await mongoose.connect(process.env.AtlasURI,{
+        await mongoose.connect(process.env.MongoURI,{
             family: 4,
         })
         console.log('Database connection successful')
